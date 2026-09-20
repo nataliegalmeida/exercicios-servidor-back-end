5d4827ef5d395c58de2029635be9833755c1bba9
Natalie ALmeida <natalie.galmeida@gmail.com>
2026-09-20 12:29:26 -0300

    Adicionar o as function para criar novas mulheres, atualização do get e criação do push

--- servidor-mulheres.js
+++ servidor-mulheres.js
@@ -1,12 +1,15 @@
-const express = require ("express")
-const router = express.Router()
+const express = require ("express")  //Criação da requisição do express
+const router = express.Router() // Criação da primeira rota
+const {v4: uuidv4} = require('uuid')
 
-const app = express()
-const porta = 3333
+const app = express() //Inicio do app
+const porta = 3333 // Criação da porta
+//aqui estou criando lista inicial de mulheres
 const mulheres = [
 
+    // Criação da primeira lista de mulheres
     {
-
+   id:'1',
    nome: 'Simara Conceição',    
    imagem: 'https://bit.ly/3LJIyOF',
    minibio: 'Desenvolvedora e instrutora',
@@ -14,14 +17,14 @@ const mulheres = [
  },
 
  {
-
-   nome: 'Iana Chan',
-   imagem: 'https://bit.ly/3JCXBqP',
-   minibio: 'CEO & Founder da PrograMaria',
+    id:'2',
+    nome: 'Iana Chan',
+    imagem: 'https://bit.ly/3JCXBqP',
+    minibio: 'CEO & Founder da PrograMaria',
 
  },
 
- {
+ {  id:'3',
     nome: 'Luana Pimentel',
     imagem: 'https://bit.ly/3FKpFaz',
     minibio: 'Senior Staff Software Engineer',
@@ -29,14 +32,35 @@ const mulheres = [
 
 ]
 
+//GET
+
 function mostraMulheres(request,response) {
 
     response.json(mulheres)
 }
 
+//POST
+
+function criaMulher(request,response){
+    const novaMulher ={
+        id:uuidv4(),
+        nome:request.body.nome,
+        imagem:request.body.image,
+        minibio:request.body.minibio
+    }
+  mulheres.push(novaMulher)
+  response.json(mulheres)
+}
+
+// PORTA
+
 function mostraPorta (){
     console.log ("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres',mostraMulheres))
-app.listen(porta, mostraPorta)
+app.use(router.get('/mulheres',mostraMulheres)) // rota GET/mulheres
+app.use(router.post('/mulheres)', criaMulher)// configuração da rota POST/mulheres
+app.listen(porta, mostraPorta) // servidor ouvindo a porta 
+
+
+ 
